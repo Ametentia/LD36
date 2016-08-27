d87fcf05314088251649db6cc5ecea7bdddd0937
WaslamAslam <waslam9@live.co.uk>
2016-08-27 18:12:11 +0100

    Enemy Stuff

--- core/src/com/perceptiongames/engine/Entities/Enemy.java
+++ core/src/com/perceptiongames/engine/Entities/Enemy.java
@@ -1,11 +1,37 @@
 package com.perceptiongames.engine.Entities;
 
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.perceptiongames.engine.Handlers.Animation;
+
+import static com.badlogic.gdx.math.MathUtils.floor;
+import static java.lang.Math.sin;
 
 public class Enemy extends Entity {
 
+    @Override
+
+    public void update(float dt) {
+        super.update(dt);
+
+        float ticker = 0;
+
+        Vector2 Pos = getPosition();
+        float PosX = Pos.x;
+        float PosY = Pos.y;
+
+        float X = floor(PosX/80);
+        float Y = floor(PosY/80);
+
+        PosX += 60*MathUtils.sin(ticker);
+
+        ticker += 1/60;
+
+    }
+
     public Enemy(Animation animation, String animationName, AABB aabb) {
         super(animation, animationName, aabb);
+
 
     }
 }
